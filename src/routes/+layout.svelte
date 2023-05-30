e95e888779a550de11d0d958fd69f49bdafa8327
Sourav Singh <commitsovercoffee@gmail.com>
2023-05-30 10:59:26 +0530

    adds key press detection

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="border-sky-500 border-2">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	let keyboard = ['a', 'b', 'c', 'd', 'e'];
+	let log = '';
+	let key;
+</script>
+
+<svelte:window
+	on:keydown={function (event) {
+		key = event.key;
+		log += key;
+	}}
+	on:keyup={function () {
+		setTimeout(function () {
+			key = null;
+		}, 100);
+	}}
+/>
+
+<div class="mx-auto w-80">
+	<p class="bg-gray-600 w-80 h-80 text-zinc-50">{log}</p>
+
+	{#each keyboard as k}
+		<button
+			class={key === k
+				? 'text-zinc-300 px-4 py-2 m-2 rounded-xl border-2 bg-zinc-900 border-zinc-500 duration-300 ease-in-out'
+				: log.includes(k)
+				? 'text-zinc-900 px-4 py-2 m-2 rounded-xl border-2 bg-teal-300 border-zinc-500 duration-300 ease-in-out'
+				: 'text-zinc-900 px-4 py-2 m-2 rounded-xl border-2 bg-zinc-300 border-zinc-500 duration-300 ease-in-out'}
+			>{k}</button
+		>
+	{/each}
+</div>
